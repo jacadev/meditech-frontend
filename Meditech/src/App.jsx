52284d40ecc223cde918be54d0587ddaf83d03e7
franco <franko_millonarios@outlook.es>
2023-04-12 14:34:33 -0300

    estructura con vite

---
+++ Meditech/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      
+     <h1>ESTA ES MI PAGINA</h1>
+     
+    </div>
+  )
+}
+
+export default App
