e0949388abb8009f3e8c9982333048dc641622a1
Filleral Mahecha <filleral19@gmail.com>
2023-04-15 13:51:52 -0500

    navbar medio terminado

--- Meditech/src/App.jsx
+++ Meditech/src/App.jsx
@@ -1,25 +1,28 @@
-import { Route, Switch } from 'react-router-dom'
-import home from './Views/Home/home'
-import services from './Views/Services/services'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './resources/theme'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/App.css";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import AuthLayout from "./layouts/auth";
+import AdminLayout from "./layouts/admin";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
+import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
 
-function App() {
-
-
-  return (
-    <div>
-  
-     
-    <ChakraProvider theme={theme}>
-      <Switch>
-       <Route exact path= "/" component={home}/>
-       <Route exact path= "/services" component={services}/>
-      </Switch>
-    </ChakraProvider>
-   
-    </div>
-  )
-}
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <ThemeEditorProvider>
+        <HashRouter>
+          <Switch>
+            <Route path={`/auth`} component={AuthLayout} />
+            <Route path={`/admin`} component={AdminLayout} />
+            <Redirect from='/' to='/admin' />
+          </Switch>
+        </HashRouter>
+      </ThemeEditorProvider>
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById("root")
+);
 
 export default App
--- Meditech/src/Views/Services/services.jsx
+++ Meditech/src/Views/Services/services.jsx
@@ -1,37 +1,26 @@
-import { Grid,GridItem } from '@chakra-ui/react'
-import CardServices from '../../Components/CardServices/cardservices';
+import Layout from "./../../Layout";
+import CardServices from "./../../Components/CardServices/cardservices";
+import { Box } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
-const services = () =>{
-
-    return(
-<Grid
-  templateAreas={`"header header"
+const home = () => {
+  return (
+    <Layout>
+      <Grid
+      templateAreas={`"header header"
                   "nav main"
                   "nav footer"`}
-  gridTemplateRows={'70px 1fr 30px'}
-  gridTemplateColumns={'250px 1fr'}
-  h='960px'
-  gap='1'
->
-  <GridItem pl='2' bg='orange.300' area={'header'}>
-    Header
-  </GridItem>
-  <GridItem pl='2' bg='pink.300' area={'nav'}>
-    Nav
-  </GridItem>
-  <GridItem pl='2' bg='pallette.color5' area={'main'}>
-    
+      gridTemplateRows={"70px 1fr 30px"}
+      gridTemplateColumns={"250px 1fr"}
+      h="960px"
+      gap="1"
+    >
+      <GridItem pl="2" bg="pallette.color5" area={"main"}>
+        <CardServices />
+      </GridItem>
+    </Grid>
+    </Layout>
+  );
+};
 
- <CardServices/>
-   
-  
-  </GridItem>
-  <GridItem pl='2' bg='pallette.color2' area={'footer'}>
-    Footer
-  </GridItem>
-</Grid>
-
-    )
-}
-
-export default services;
+export default home;
