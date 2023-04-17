beecf4f958c71404a989f43ab7a6ba58e3ea4ce1
carlosnr9 <carlosnr9@gmail.com>
2023-04-17 11:01:47 -0500

    agregado el paginado a Specialist.jsx

--- Meditech/src/Views/Especialista/Specialists.jsx
+++ Meditech/src/Views/Especialista/Specialists.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, SimpleGrid } from "@chakra-ui/react";
 import SpecialistCard from "../../components/Especialista/SpecialistCard";
 import SpecialistsData from "../../data/DataSpecialist";
 
@@ -9,33 +9,31 @@ const Specialists = () => {
   const [filterCity, setFilterCity] = useState("all");
   const [sort, setSort] = useState("asc");
   const [sort2, setSort2] = useState("asc");
-  const [searchTerm, setSearchTerm] = useState('');   // ***********************************************
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1); // Estado para la paginación
 
-  
+  //***************************** */
+  const ELEMENTOS_POR_PAGINA = 10;
+  const startIndex = (currentPage - 1) * ELEMENTOS_POR_PAGINA;
+  const endIndex = startIndex + ELEMENTOS_POR_PAGINA;
+  //***************************** */
 
   useEffect(() => {
     // Simulación de llamada a API o método para obtener datos de especialistas y ciudades
     setSpecialists(SpecialistsData);
   }, []);
 
-
-
-  //  modificar todoeste codigo para que funcione el buscador
   const filteredSpecialists = specialists
-  .filter(
-    (spec) =>
-      filterSpecialty === "" ? true : spec.specialty === filterSpecialty
-  )
-  .filter((spec) => (filterCity === "all" ? true : spec.city === filterCity))
-  .filter((spec) =>
-    searchTerm === ""
-      ? true
-      : spec.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  //*********************************************** */
-
-
-
+    .filter(
+      (spec) =>
+        filterSpecialty === "" ? true : spec.specialty === filterSpecialty
+    )
+    .filter((spec) => (filterCity === "all" ? true : spec.city === filterCity))
+    .filter((spec) =>
+      searchTerm === ""
+        ? true
+        : spec.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
   const sortedSpecialists = [...filteredSpecialists].sort((a, b) =>
     sort === "asc"
@@ -57,83 +55,91 @@ const Specialists = () => {
   const handleSortClick2 = () => setSort2(sort2 === "asc" ? "desc" : "asc");
 
   return (
-    <><Box>
-      <Box mb="4">
-        <Text fontSize="lg" fontWeight="bold">
-          Filtrar por especialidad:
-        </Text>
-        <select value={filterSpecialty} onChange={handleFilterSpecialtyChange}>
-          <option value="">Todas las especialidades</option>
-          <option value="Cardiología">Cardiología</option>
-          <option value="Dermatología">Dermatología</option>
-          <option value="Ginecología">Ginecología</option>
-          <option value="Medicina General">Medicina General</option>
-          <option value="Neurología">Neurología</option>
-          <option value="Nutrición">Nutrición</option>
-          <option value="Odontología">Odontología</option>
-          <option value="Oftalmología">Oftalmologia</option>
-          <option value="Pediatría">Pediatría</option>
-          <option value="Psicología">Psicología</option>
-        </select>
-      </Box>
-      <Box mb="4">
-        <Text fontSize="lg" fontWeight="bold">
-          Filtrar por ciudad:
-        </Text>
-        <select value={filterCity} onChange={handleFilterCityChange}>
-          <option value="all">Todas las ciudades</option>
-          <option value="Barranquilla">Barranquilla</option>
-          <option value="Bogota">Bogotá</option>
-          <option value="Cali">Cali</option>
-          <option value="Cartagena">Cartagena</option>
-          <option value="Medellin">Medellín</option>
-        </select>
-      </Box>
+    <>
+      <Box>
+        <Box mb="4">
+          <Text fontSize="lg" fontWeight="bold">
+            Filtrar por especialidad:
+          </Text>
+          <select value={filterSpecialty} onChange={handleFilterSpecialtyChange}>
+            <option value="">Todas las especialidades</option>
+            <option value="Cardiología">Cardiología</option>
+            <option value="Dermatología">Dermatología</option>
+            <option value="Ginecología">Ginecología</option>
+            <option value="Medicina General">Medicina General</option>
+            <option value="Neurología">Neurología</option>
+            <option value="Nutrición">Nutrición</option>
+            <option value="Odontología">Odontología</option>
+            <option value="Oftalmología">Oftalmologia</option>
+            <option value="Pediatría">Pediatría</option>
+            <option value="Psicología">Psicología</option>
+          </select>
+        </Box>
+        <Box mb="4">
+          <Text fontSize="lg" fontWeight="bold">
+            Filtrar por ciudad:
+          </Text>
+          <select value={filterCity} onChange={handleFilterCityChange}>
+            <option value="all">Todas las ciudades</option>
+            <option value="Barranquilla">Barranquilla</option>
+            <option value="Bogota">Bogotá</option>
+            <option value="Cali">Cali</option>
+            <option value="Cartagena">Cartagena</option>
+            <option value="Medellin">Medellín</option>
+          </select>
+        </Box>
 
-      <Box mb="4">
-        <Text fontSize="lg" fontWeight="bold">
-          Ordenar por precio:
-        </Text>
-        <Button onClick={handleSortClick}>
-          {sort === "asc" ? "De menor a mayor" : "De mayor a menor"}
+        <Box mb="4">
+          <Text fontSize="lg" fontWeight="bold">
+            Ordenar por precio:
+          </Text>
+          <Button onClick={handleSortClick}>
+            {sort === "asc" ? "De menor a mayor" : "De mayor a menor"}
+          </Button>
+        </Box>
+
+        <Box mb="4">
+          <Text fontSize="lg" fontWeight="bold">
+            Ordenar por popularidad:
+          </Text>
+          <Button onClick={handleSortClick2}>
+            {sort2 === "asc" ? "De mayor a menor" : "De menor a mayor"}
+          </Button>
+        </Box>
+
+        <Box mb="4">
+          <Text fontSize="lg" fontWeight="bold">
+            Buscar Especialista:
+          </Text>
+          <input
+            type="text"
+            placeholder="Buscar por nombre de especialista"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </Box>
+
+        <SimpleGrid columns={2} spacing={6}>
+          {sortedRating.slice(startIndex, endIndex).map((spec) => (
+            <SpecialistCard key={spec.id} specialist={spec} />
+          ))}
+        </SimpleGrid>
+
+        <Button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+        >
+          Página anterior
         </Button>
-      </Box>
-
-      <Box mb="4">
-        <Text fontSize="lg" fontWeight="bold">
-          Ordenar por popularidad:
-        </Text>
-        <Button onClick={handleSortClick2}>
-          {sort2 === "asc" ? "De mayor a menor" : "De menor a mayor"}
+        <Button
+          disabled={endIndex >= sortedRating.length}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          Siguiente página
         </Button>
+
       </Box>
-
-       { /*agregar este BOX + input} */  }
-
-      <Box mb="4">
-        <Text fontSize="lg" fontWeight="bold">
-          Buscar Especialista:
-        </Text>
-        <input
-          type="text"
-          placeholder="Buscar por nombre de especialista"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)} />
-      </Box>
-
-      { /*HASTA AQUI*/  }
-
-      {sort2 === "asc"
-        ? sortedSpecialists.map((spec) => (
-          <SpecialistCard key={spec.id} specialist={spec} />
-        ))
-        : sortedRating.map((spec) => (
-          <SpecialistCard key={spec.id} specialist={spec} />
-        ))}
-    </Box>
     </>
-
-
   );
 };
 
