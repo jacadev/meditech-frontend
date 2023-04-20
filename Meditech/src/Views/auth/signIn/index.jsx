c96dabccc0656019af9d7ffc1ff9a2740dc0cb0b
Filleral Mahecha <filleral19@gmail.com>
2023-04-19 20:53:56 -0500

    login

--- Meditech/src/Views/auth/signIn/index.jsx
+++ Meditech/src/Views/auth/signIn/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+import { useHistory } from "react-router-dom";
 // Chakra imports
 import {
   Box,
@@ -26,6 +28,7 @@ import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { RiEyeCloseLine } from "react-icons/ri";
 
 function SignIn() {
+  const history = useHistory();
   // Chakra color mode
   const textColor = useColorModeValue("navy.700", "white");
   const textColorSecondary = "gray.400";
@@ -44,106 +47,114 @@ function SignIn() {
   );
   const [show, setShow] = React.useState(false);
   const handleClick = () => setShow(!show);
+  const responseGoogle = (response) => {
+    const userName = response.profileObj.name;
+    // Redirecciona al usuario a la ruta deseada
+    history.push("/admin/default");
+    // Muestra el nombre del usuario en algún lugar de la interfaz de usuario
+  };
+  const onFailure = (error) => {
+    console.log(error);
+    // Aquí puedes manejar el error del inicio de sesión de Google
+  };
   return (
     <DefaultAuth illustrationBackground={illustration} image={illustration}>
       <Flex
         maxW={{ base: "100%", md: "max-content" }}
-        w='100%'
+        w="100%"
         mx={{ base: "auto", lg: "0px" }}
-        me='auto'
-        h='100%'
-        alignItems='start'
-        justifyContent='center'
+        me="auto"
+        h="100%"
+        alignItems="start"
+        justifyContent="center"
         mb={{ base: "30px", md: "60px" }}
         px={{ base: "25px", md: "0px" }}
         mt={{ base: "40px", md: "14vh" }}
-        flexDirection='column'>
-        <Box me='auto'>
-          <Heading color={textColor} fontSize='36px' mb='10px'>
+        flexDirection="column"
+      >
+        <Box me="auto">
+          <Heading color={textColor} fontSize="36px" mb="10px">
             Sign In
           </Heading>
           <Text
-            mb='36px'
-            ms='4px'
+            mb="36px"
+            ms="4px"
             color={textColorSecondary}
-            fontWeight='400'
-            fontSize='md'>
+            fontWeight="400"
+            fontSize="md"
+          >
             Enter your email and password to sign in!
           </Text>
         </Box>
         <Flex
-          zIndex='2'
-          direction='column'
+          zIndex="2"
+          direction="column"
           w={{ base: "100%", md: "420px" }}
-          maxW='100%'
-          background='transparent'
-          borderRadius='15px'
+          maxW="100%"
+          background="transparent"
+          borderRadius="15px"
           mx={{ base: "auto", lg: "unset" }}
-          me='auto'
-          mb={{ base: "20px", md: "auto" }}>
-          <Button
-            fontSize='sm'
-            me='0px'
-            mb='26px'
-            py='15px'
-            h='50px'
-            borderRadius='16px'
-            bg={googleBg}
-            color={googleText}
-            fontWeight='500'
-            _hover={googleHover}
-            _active={googleActive}
-            _focus={googleActive}>
-            <Icon as={FcGoogle} w='20px' h='20px' me='10px' />
-            Sign in with Google
-          </Button>
-          <Flex align='center' mb='25px'>
+          me="auto"
+          mb={{ base: "20px", md: "auto" }}
+        >
+          <GoogleLogin
+            clientId="486483669928-rdvcde4ja0g9diu12md4bpf6ts4bj2d6.apps.googleusercontent.com"
+            buttonText="Sign in with Google"
+            onSuccess={responseGoogle}
+            onFailure={onFailure}
+            cookiePolicy={"single_host_origin"}
+          >
+            
+          </GoogleLogin>
+          <Flex align="center" mb="25px">
             <HSeparator />
-            <Text color='gray.400' mx='14px'>
+            <Text color="gray.400" mx="14px">
               or
             </Text>
             <HSeparator />
           </Flex>
           <FormControl>
             <FormLabel
-              display='flex'
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
+              display="flex"
+              ms="4px"
+              fontSize="sm"
+              fontWeight="500"
               color={textColor}
-              mb='8px'>
+              mb="8px"
+            >
               Email<Text color={brandStars}>*</Text>
             </FormLabel>
             <Input
               isRequired={true}
-              variant='auth'
-              fontSize='sm'
+              variant="auth"
+              fontSize="sm"
               ms={{ base: "0px", md: "0px" }}
-              type='email'
-              placeholder='mail@simmmple.com'
-              mb='24px'
-              fontWeight='500'
-              size='lg'
+              type="email"
+              placeholder="mail@simmmple.com"
+              mb="24px"
+              fontWeight="500"
+              size="lg"
             />
             <FormLabel
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
+              ms="4px"
+              fontSize="sm"
+              fontWeight="500"
               color={textColor}
-              display='flex'>
+              display="flex"
+            >
               Password<Text color={brandStars}>*</Text>
             </FormLabel>
-            <InputGroup size='md'>
+            <InputGroup size="md">
               <Input
                 isRequired={true}
-                fontSize='sm'
-                placeholder='Min. 8 characters'
-                mb='24px'
-                size='lg'
+                fontSize="sm"
+                placeholder="Min. 8 characters"
+                mb="24px"
+                size="lg"
                 type={show ? "text" : "password"}
-                variant='auth'
+                variant="auth"
               />
-              <InputRightElement display='flex' alignItems='center' mt='4px'>
+              <InputRightElement display="flex" alignItems="center" mt="4px">
                 <Icon
                   color={textColorSecondary}
                   _hover={{ cursor: "pointer" }}
@@ -152,56 +163,61 @@ function SignIn() {
                 />
               </InputRightElement>
             </InputGroup>
-            <Flex justifyContent='space-between' align='center' mb='24px'>
-              <FormControl display='flex' alignItems='center'>
+            <Flex justifyContent="space-between" align="center" mb="24px">
+              <FormControl display="flex" alignItems="center">
                 <Checkbox
-                  id='remember-login'
-                  colorScheme='brandScheme'
-                  me='10px'
+                  id="remember-login"
+                  colorScheme="brandScheme"
+                  me="10px"
                 />
                 <FormLabel
-                  htmlFor='remember-login'
-                  mb='0'
-                  fontWeight='normal'
+                  htmlFor="remember-login"
+                  mb="0"
+                  fontWeight="normal"
                   color={textColor}
-                  fontSize='sm'>
+                  fontSize="sm"
+                >
                   Keep me logged in
                 </FormLabel>
               </FormControl>
-              <NavLink to='/auth/forgot-password'>
+              <NavLink to="/auth/forgot-password">
                 <Text
                   color={textColorBrand}
-                  fontSize='sm'
-                  w='124px'
-                  fontWeight='500'>
+                  fontSize="sm"
+                  w="124px"
+                  fontWeight="500"
+                >
                   Forgot password?
                 </Text>
               </NavLink>
             </Flex>
             <Button
-              fontSize='sm'
-              variant='brand'
-              fontWeight='500'
-              w='100%'
-              h='50'
-              mb='24px'>
+              fontSize="sm"
+              variant="brand"
+              fontWeight="500"
+              w="100%"
+              h="50"
+              mb="24px"
+            >
               Sign In
             </Button>
           </FormControl>
           <Flex
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='start'
-            maxW='100%'
-            mt='0px'>
-            <Text color={textColorDetails} fontWeight='400' fontSize='14px'>
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="start"
+            maxW="100%"
+            mt="0px"
+          >
+            <Text color={textColorDetails} fontWeight="400" fontSize="14px">
               Not registered yet?
-              <NavLink to='/auth/sign-up'>
+              <NavLink to="/auth/sign-up">
                 <Text
                   color={textColorBrand}
-                  as='span'
-                  ms='5px'
-                  fontWeight='500'>
+                  as="span"
+                  ms="5px"
+                  fontWeight="500"
+                >
                   Create an Account
                 </Text>
               </NavLink>
