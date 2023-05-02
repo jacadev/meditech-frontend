5f2d49cd2d16ad4769c6674238bd471aaac6b6a0
AdrianZarate <adrian.1478.rz@gmail.com>
2023-05-01 21:29:47 -0500

    filtro de especialidades

--- Meditech/src/Redux/Reducer/reducer.jsx
+++ Meditech/src/Redux/Reducer/reducer.jsx
@@ -1,4 +1,4 @@
-import { POST_RESERVE, POST_RESERVE_ERROR,FORM_DATA, GET_DOCTOR, CLEAN_DATAIL_ID, GET_DOCTORS, GET_DOCTORS_NAME, GET_APPOINTMENT_PATIENT, GET_PATIENT } from '../Actions/actions-types';
+import { POST_RESERVE, POST_RESERVE_ERROR,FORM_DATA, GET_DOCTOR, CLEAN_DATAIL_ID, GET_DOCTORS, GET_DOCTORS_NAME, GET_APPOINTMENT_PATIENT, GET_PATIENT, GET_SPECIALTIES } from '../Actions/actions-types';
 import {
   SIGNIN_USER,
   SIGNUP_USER,
@@ -23,7 +23,8 @@ const initialState = {
   doctorDetail: [],
   doctors: [],
   appointmentOfPatientID:[], // aca se guardan los datos del paciente que se traen de la DB para el componente que se encarga de visualizar las citas del paciente 
-  infoPatient: []
+  infoPatient: [],
+  specialties: []
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -136,6 +137,11 @@ const rootReducer = (state = initialState, action) => {
         return {
           ...state,
           infoPatient: action.payload
+        }
+      case GET_SPECIALTIES:
+        return {
+          ...state,
+          specialties: action.payload
         }
     default:
       return state;
